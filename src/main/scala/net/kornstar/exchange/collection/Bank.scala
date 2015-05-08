eafd75cb6ad67a1407e4ac23b720ba59b02488a0
U-TheBox\Ben Kornmeier <ben@kornstar.net>
2015-05-07 20:46:31 -0600

    changes for the bank

--- src/main/scala/net/kornstar/exchange/collection/Bank.scala
+++ src/main/scala/net/kornstar/exchange/collection/Bank.scala
@@ -1,10 +1,21 @@
 package net.kornstar.exchange.collection
 
+import org.slf4j.LoggerFactory
+
 /**
  * Created by Ben Kornmeier on 5/7/2015.
  */
+object Transaction {
+  sealed trait TransactionType
+  object TransactionType {
+    case object Deposit extends TransactionType
+    case object Withdraw extends TransactionType
+    case object Buy extends TransactionType
+    case object Sell extends TransactionType
+  }
+}
 
-case class Transaction(id:Int,userId:Int,isBid:Boolean,baseCurrencyAmount:Double, otherCurrencyAmount:Double,orderOpt:Option[Order] = None)
+case class Transaction(id:Int,userId:Int,isBid:Boolean,baseCurrencyAmount:Double, otherCurrencyAmount:Double,transactionType:Transaction.TransactionType,note:Option[String] = None,orderOpt:Option[Order] = None)
 
 trait Account {
   def baseCurrencyAmount:Double
@@ -13,8 +24,8 @@ trait Account {
   def submitTransaction(t:Transaction):Account
 }
 
-class MemoryAccount(userId:Int,ledger:Seq[Transaction] = Seq.empty[Transaction]) extends Account {
-  protected def sumLedger(isBase:Boolean):Double = {
+case class MemoryAccount(userId:Int,ledger:Seq[Transaction] = Seq.empty[Transaction]) extends Account {
+  private def sumLedger(isBase:Boolean):Double = {
     ledger.foldLeft(0.0) {
       case (acc,ele) if isBase =>
         acc + ele.baseCurrencyAmount
@@ -27,11 +38,16 @@ class MemoryAccount(userId:Int,ledger:Seq[Transaction] = Seq.empty[Transaction])
 
   lazy val otherCurrencyAmount = sumLedger(false)
 
+  def submitTransaction(t:Transaction):Account = {
+    this.copy(ledger = ledger :+ t)
+  }
+
 }
 trait Bank {
   def getAccountFor(userId:Int):Option[Account]
 }
 object Bank {
+  val logger = LoggerFactory.getLogger(classOf[Bank])
 }
 class MemoryBank(userIdToAccount:Map[Int,Account] = Map.empty[Int,Account]) extends Bank {
   import Bank._
@@ -42,7 +58,10 @@ class MemoryBank(userIdToAccount:Map[Int,Account] = Map.empty[Int,Account]) exte
   def submitTransaction(t:Transaction):Bank = {
     getAccountFor(t.userId).map{acct =>
       new MemoryBank(userIdToAccount + (t.userId -> acct.submitTransaction(t)))
-    } getOrElse(this)
+    } getOrElse{
+      logger.warn(s"Transaction user ID not found ${t}")
+      this
+    }
   }
 
 
