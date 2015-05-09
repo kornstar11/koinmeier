8d68febf15da0359a271a5d752094838dfaa3a7b
U-TheBox\Ben Kornmeier <ben@kornstar.net>
2015-05-09 14:35:34 -0600

    broke bank into seperate files

--- src/main/scala/net/kornstar/exchange/collection/Bank.scala
+++ src/main/scala/net/kornstar/exchange/collection/Bank.scala
@@ -11,88 +11,8 @@ import scala.util.Try
 /**
  * Created by Ben Kornmeier on 5/7/2015.
  */
-object Transaction {
-  sealed trait TransactionType
-  object TransactionType {
-    case object Deposit extends TransactionType
-    case object Withdraw extends TransactionType
-    case object Buy extends TransactionType
-    case object Sell extends TransactionType
-  }
-}
 
-case class Transaction(userId:Int,baseCurrencyAmount:Double, otherCurrencyAmount:Int,transactionType:Transaction.TransactionType,time:Option[Long] = None,id:Option[Int] = None,note:Option[String] = None,orderOpt:Option[Order] = None)
 
-trait Account {
-  def baseCurrencyAmount:Double
-  def otherCurrencyAmount:Double
-
-  def getLedger:Iterable[Transaction]
-
-  def submitTransaction(t:Transaction):Account
-
-  def createOrder(price:Double,amount:Int,isBid:Boolean):(Account,Order)
-
-  def userId:Int
-
-}
-
-object Account {
-  val logger = LoggerFactory.getLogger(classOf[Account])
-  val idMaker:AtomicInteger = new AtomicInteger(0)
-  def apply(userId:Int):Account = {
-    logger.info(s"Creating a new account for user ID ${userId}")
-    Try(config.getString("account-type")).getOrElse("memory") match {
-      case "memory" => new MemoryAccount(userId)
-      case _ => throw new IllegalArgumentException("Unknown account type.")
-    }
-  }
-}
-
-case class MemoryAccount(val userId:Int,ledger:Seq[Transaction] = Seq.empty[Transaction]) extends Account {
-  import Account._
-  private def sumLedger(isBase:Boolean):Double = {
-    ledger.foldLeft(0.0) {
-      case (acc,ele) if isBase =>
-        acc + ele.baseCurrencyAmount
-      case (acc,ele) =>
-        acc + ele.otherCurrencyAmount
-    }
-  }
-
-  def getLedger = ledger
-
-  lazy val baseCurrencyAmount = sumLedger(true)
-
-  lazy val otherCurrencyAmount = sumLedger(false)
-
-  def submitTransaction(t:Transaction):Account = {
-    val tWithId = if(t.id.isEmpty) t.copy(id = Some(idMaker.getAndIncrement))
-                  else t
-    val tWithTime = if(tWithId.time.isEmpty) tWithId.copy(time = Some(System.currentTimeMillis()))
-                    else tWithId
-
-    this.copy(ledger = ledger :+ tWithTime)
-  }
-
-  def createOrder(price:Double,amount:Int,isBid:Boolean):(Account,Order) = {
-    val amountInDrawedFromLedger = if(isBid) baseCurrencyAmount
-                               else otherCurrencyAmount
-
-    val proposedTotalPrice = price * amount.toDouble
-
-    assert(proposedTotalPrice <= amountInDrawedFromLedger, s"unable to make a order of ${proposedTotalPrice} because only ${amountInDrawedFromLedger} is avialiable.")
-
-    val newAcct = if(isBid) submitTransaction(Transaction(userId,proposedTotalPrice * -1.0,0,TransactionType.Buy,Some(System.currentTimeMillis()),Some(idMaker.getAndIncrement)))
-                  else submitTransaction(Transaction(userId, 0.0, (proposedTotalPrice * -1.0).toInt, TransactionType.Sell, Some(System.currentTimeMillis()), Some(idMaker.getAndIncrement)))
-
-    val order = Order(userId,isBid,amount,price)
-
-    newAcct -> order
-
-  }
-
-}
 trait Bank {
   def getAccountFor(userId:Int):Option[Account]
 
---
+++ src/main/scala/net/kornstar/exchange/collection/Transaction.scala
@@ -0,0 +1,17 @@
+package net.kornstar.exchange.collection
+
+/**
+ * Created by Ben Kornmeier on 5/9/2015.
+ */
+object Transaction {
+  sealed trait TransactionType
+  object TransactionType {
+    case object Deposit extends TransactionType
+    case object Withdraw extends TransactionType
+    case object Buy extends TransactionType
+    case object Sell extends TransactionType
+  }
+}
+
+case class Transaction(userId:Int,baseCurrencyAmount:Double, otherCurrencyAmount:Int,transactionType:Transaction.TransactionType,time:Option[Long] = None,id:Option[Int] = None,note:Option[String] = None,orderOpt:Option[Order] = None)
+
